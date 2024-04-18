cdb9292d9cb3eb50bef91ca314b372998c6d373e
Arash <arash.negahdari89@gmail.com>
2024-04-18 16:28:18 +0330

    Setup cart and add and display items to cart based on button's id

--- source-code/app.js
+++ source-code/app.js
@@ -38,12 +38,61 @@ class UI {
     });
     productsDOM.innerHTML = results;
   }
+  
+  addToCartBtns() {
+    cartButtons = [...document.querySelectorAll(".add-to-cart")];
+    cartButtons.forEach((btn) => {
+      //get all buttons id after rendering to the DOM
+      const btnIds = Number(btn.dataset.id);
+
+      //item validation by ID
+      const isInCart = cart.find((p) => p.id === btnIds);
+      if (isInCart) {
+        btn.textContent = "Added";
+        btn.disabled = true;
+        btn.blur();
+      }
+
+      btn.addEventListener("click", (event) => {
+        event.target.textContent = "Added";
+        event.target.disabled = true;
+        event.target.blur();
+
+        //get the product using id
+        const productsAdded = {
+          ...Storage.getProductsById(Number(btnIds)), //OR - event.target.dataset.id
+          quantity: 1,
+        };
+
+        //add to cart
+        cart = [...cart, productsAdded];
+
+        //save to local storage
+        Storage.saveCart(cart);
+
+        //update cart value
+        this.setCartValue(cart);
+
+        //add cart item
+        this.addCartItems(productsAdded);
+      });
+    });
+  }
 }
 
 //*3. Store the Products in Local Storage
 class Storage {
   static saveProducts(products) {
     localStorage.setItem("Products", JSON.stringify(products)) || [];
+  }
+
+  static getProductsById(id) {
+    const _products = JSON.parse(localStorage.getItem("Products"));
+    return _products.find((p) => p.id === id); //returns an object
+  }
+
+  static saveCart(cart) {
+    localStorage.setItem("CartItems", JSON.stringify(cart)) || [];
   }
 }
 
